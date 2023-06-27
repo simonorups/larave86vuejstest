34d20ca6f26b4e566f2f6cb5d5c34b3bd8016d35
Simon Orupia <sorupia@gmail.com>
2023-06-27 06:24:50 +0300

    albums set up

--- app/Http/Controllers/API/AlbumController.php
+++ app/Http/Controllers/API/AlbumController.php
@@ -6,8 +6,13 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Album;
 
+use Illuminate\Http\Client\Pool;
+use Illuminate\Support\Facades\Http;
+
 class AlbumController extends Controller
 {
+    private const LAST_FM_URL = 'http://ws.audioscrobbler.com/2.0/?api_key=b703f6c39ed526249165ee98d6e06f39&format=json&';//
+
     /**
      * Display a listing of the resource.
      *
@@ -15,8 +20,10 @@ class AlbumController extends Controller
      */
     public function index()
     {
-        $albums = Album::all()->toArray();
-        return array_reverse($albums);
+        //dd(auth()->user());
+        $albums = Album::where('user_id', auth()->user()->id)->latest()->get();
+        //dd($albums);
+        return $albums;
     }
 
     /**
@@ -27,12 +34,21 @@ class AlbumController extends Controller
      */
     public function store(Request $request)
     {
+        //dd($request->input('name'), $request->query('name'));
+        /* dd(session("loggedInUser"));
+        $loggedInUser = [];
+        if ($request->session()->has('loggedInUser')) {
+            $loggedInUser = session("loggedInUser");
+        }
+
+        dd(Auth::user(), $loggedInUser); */
+
         $album = new Album([
-            'title' => $request->input('title'),
-            'description' => $request->input('description')
+            'name' => $request->input('name'),
+            'user_id' => auth()->user()->id,
         ]);
         $album->save();
- 
+
         return response()->json('The album successfully added');
     }
 
@@ -48,15 +64,6 @@ class AlbumController extends Controller
         return response()->json($album);
     }
 
-
-    // edit album
-    public function edit($id)
-    {
-        $album = Album::find($id);
-        return response()->json($album);
-    }
-
-    
     /**
      * Update the specified resource in storage.
      *
@@ -68,7 +75,7 @@ class AlbumController extends Controller
     {
         $album = Album::find($id);
         $album->update($request->all());
- 
+
         return response()->json('The album successfully updated');
     }
 
@@ -82,7 +89,56 @@ class AlbumController extends Controller
     {
         $album = Album::find($id);
         $album->delete();
- 
+
         return response()->json('The album successfully deleted');
+    }
+
+    // edit album
+    public function search($name)
+    {
+        $search     = "method=album.Search&album=".$name."&limit=1";
+        $getSimilar = "method=album.getSimilar&album=".$name;
+        $getTopTracks = "method=album.getTopTracks&album=".$name;
+        $getTopAlbums = "method=album.getTopAlbums&album=".$name;
+
+        //var_dump(self::LAST_FM_URL.$search, self::LAST_FM_URL.$getSimilar, self::LAST_FM_URL.$getTopTracks, self::LAST_FM_URL.$getTopAlbums);
+
+        $album = $response = [];
+
+        //execute concurrently for performance
+        $poolResponse = Http::pool(fn (Pool $pool) => [
+            $pool->get(self::LAST_FM_URL.$search),
+            $pool->get(self::LAST_FM_URL.$getSimilar),
+            $pool->get(self::LAST_FM_URL.$getTopTracks),
+            $pool->get(self::LAST_FM_URL.$getTopAlbums),
+        ]);
+
+        $response[0] = $poolResponse[0]->json();
+        $response[1] = $poolResponse[1]->json();
+        $response[2] = $poolResponse[2]->json();
+        $response[3] = $poolResponse[3]->json();
+
+        $album['name'] = $response[0]['results']['albummatches']['album'][0]['name'];
+        
+        $similaralbums = $response[1]['similaralbums']['album'];
+        foreach($similaralbums as $key=>$similaralbum){
+            $album['similaralbums'][$key] = $similaralbum["name"];
+        }
+        sort($album['similaralbums']);
+
+        $toptracks = $response[2]['toptracks']['track'];
+        foreach($toptracks as $key=>$toptrack){
+            $album['toptracks'][$key] = $toptrack["name"];
+        }
+        sort($album['toptracks']);
+
+        $topalbums = $response[3]['topalbums']['album'];
+        foreach($topalbums as $key=>$topalbum){
+            $album['topalbums'][$key] = $topalbum["name"];
+        }
+        sort($album['topalbums']);
+
+        return $album;
+
     }
 }
